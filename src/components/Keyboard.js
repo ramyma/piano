dbe0e3f2bb51a6cbde204064b3c312b0baaaae5f
l <l>
2020-05-04 16:22:11 +0200

    Keyboard component initial implementation

---
+++ src/components/Keyboard.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Key from './Key';
+
+const keys = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+
+export const isRaised = (key) => {
+  const pattern = /[A-G]b/;
+  return pattern.test(key);
+};
+
+function Keyboard() {
+  return (
+    <section id="keyboard">
+      {keys.map((k) => (
+        <Key key={k} note={k} showNote raised={isRaised(k)} />
+      ))}
+    </section>
+  );
+}
+
+Keyboard.propTypes = {};
+
+export default Keyboard;
