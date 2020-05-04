f9adc1c6e550ecf3c90614f94a6a2463ae45f127
l <l>
2020-05-04 17:20:39 +0200

    Added Keyboard component and Log component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Key from './components/Key';
+import Keyboard from './components/Keyboard';
+import Log from './components/Log';
+
+export const logReducer = (state, action) => {
+  if (action.type === 'ADD_LOG_ENTRY') {
+    return [...state, { entry: action.entry, id: action.id }];
+  }
+  return state;
+};
+
+const logInitalState = [];
 
 function App() {
+  const [logEntries, dispatchAddLogEntry] = useReducer(
+    logReducer,
+    logInitalState
+  );
+
+  const handleKeyPlayed = (key) => {
+    dispatchAddLogEntry({ type: 'ADD_LOG_ENTRY', entry: key, id: Date.now() });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +39,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Keyboard onKeyPlayed={handleKeyPlayed} />
+      {/* TODO: handle tests with multiple usages */}
+      {/* <Keyboard onKeyPlayed={handleKeyPlayed} /> */}
+      <Log logEntries={logEntries} />
     </div>
   );
 }
