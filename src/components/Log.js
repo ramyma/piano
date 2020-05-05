6b1c35fef6202d4609e2003b555a8e7eca42fb68
l <l>
2020-05-05 15:28:46 +0200

    Update Log component implementation

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Log.module.scss';
 
-export default function Log({ logEntries }) {
+export default function Log({ logEntries, clearLog }) {
   return (
-    <section>
-      <h3 id="log-label">Log</h3>
-      <ul aria-labelledby="log-label">
-        {logEntries.map(({ entry, id }) => (
-          <li key={id}>{entry}</li>
-        ))}
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <h3 id="log-label">Log</h3>
+        <button disabled={!logEntries || !logEntries.length} onClick={clearLog}>
+          Clear Log
+        </button>
+      </header>
+      <ul aria-labelledby="log-label" className={styles.log}>
+        {logEntries && logEntries.length ? (
+          logEntries.map(({ entry, id }) => <li key={id}>{entry}</li>)
+        ) : (
+          <li>No log entries yet.</li>
+        )}
       </ul>
     </section>
   );
 }
+
+Log.propTypes = {
+  logEntries: PropTypes.array,
+  clearLog: PropTypes.func,
+};
